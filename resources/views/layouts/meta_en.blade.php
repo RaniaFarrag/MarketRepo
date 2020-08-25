6743ba9dd616296c2729e41d596473af6b338059
Amr Mohammed <amrandcompany@gmail.com>
2020-08-25 11:25:25 +0200

    ar

--- resources/views/layouts/meta_en.blade.php
+++ resources/views/layouts/meta_en.blade.php
@@ -18,6 +18,7 @@
     <link href="{{ asset('dashboard/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.6') }}"  rel="stylesheet" type="text/css"/>
     <link href="{{ asset('dashboard/assets/css/style.bundle.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
 @elseif(app()->getLocale() == 'ar')
+
     <link href="{{ asset('dashboard/assets/plugins/global/plugins.bundle.css?v=7.0.6" ') }}" rel="stylesheet" type="text/css"/>
     <link href="{{ asset('dashboard/assets/plugins/custom/prismjs/prismjs.bundle.css?v=7.0.6') }}"  rel="stylesheet" type="text/css"/>
     <link href="{{ asset('dashboard/assets/css/style.bundle.css?v=7.0.6') }}" rel="stylesheet" type="text/css"/>
