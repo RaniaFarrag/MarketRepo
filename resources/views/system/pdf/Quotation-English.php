3e6f341e1ee0ba5dff5659a4df497ec51761a758
Amr Mohammed <amrandcompany@gmail.com>
2020-09-13 14:23:30 +0200

    edit Quotation-English& Arabic

--- resources/views/system/pdf/Quotation-English.php
+++ resources/views/system/pdf/Quotation-English.php
@@ -175,14 +175,30 @@
 
             <tbody>
             <tr class="en ltr">
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">TRADE</td>
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">GENDER</td>
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">QUALIFICATION</td>
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">QTY</td>
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">NATIONALITY</td>
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">SALARY (SAR)</td>
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">RECRUITMENT CHARGES PER CANDIDATE</td>
-                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">VISA PROCESSING CHARGES PER CANDIDATE(U.S $)</td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    TRADE
+                </td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    GENDER
+                </td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    QUALIFICATION
+                </td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    QTY
+                </td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    NATIONALITY
+                </td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    SALARY (SAR)
+                </td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    RECRUITMENT CHARGES PER CANDIDATE
+                </td>
+                <td class="en ltr" style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    VISA PROCESSING CHARGES PER CANDIDATE(U.S $)
+                </td>
             </tr>
             <tr>
                 <td class="en ltr" style="font-size: 12px; vertical-align: middle;">GENERAL NURSES</td>
@@ -190,7 +206,9 @@
                 <td class="en ltr" style="font-size: 12px; vertical-align: middle;">BACHELOR</td>
                 <td class="en ltr" style="font-size: 12px; vertical-align: middle;">15</td>
                 <td class="en ltr" style="font-size: 12px; vertical-align: middle;">PHILIPPINES</td>
-                <td class="en ltr" style="font-size: 12px; vertical-align: middle;">3500+300+ Transport & Accommodation</td>
+                <td class="en ltr" style="font-size: 12px; vertical-align: middle;">3500+300+ Transport &
+                    Accommodation
+                </td>
                 <td class="en ltr" style="font-size: 12px; vertical-align: middle;">ONE MONTH SALARY</td>
                 <td class="en ltr" style="font-size: 12px; vertical-align: middle;">$750</td>
             </tr>
@@ -199,16 +217,17 @@
 
         <p class="en ltr" style="line-height: 24px;">
             <strong>Terms & Conditions:-</strong><br/>
-            •	All Terms & Conditions are subjected to Signed Contract.<br/>
-            •	Accommodation & Transportation will be provided by the client.<br/>
-            •	Employee Joining Tickets will be provided by the client.<br/>
-            •	Service contract period is for 2 years and renewable as per the standard Terms & Conditions.<br/>
-            •	Working hours as per Saudi Labor Law.<br/>
-            •	Dataflow, Pro-Metric & Classification charges will be reimbursed by the client before candidate’s arrival at Actuals<br/>
-            •	Necessary Job Order to be obtained by the client from the respective consulates<br/>
-            •	100 % of the invoiced amount to be paid before candidate’s arrival<br/>
-            •	VAT and any Additional Government Taxes and Government Fee will be applicable.<br/>
-            •	This quotation is Valid for 7 Days from the issued date.<br/>
+            • All Terms & Conditions are subjected to Signed Contract.<br/>
+            • Accommodation & Transportation will be provided by the client.<br/>
+            • Employee Joining Tickets will be provided by the client.<br/>
+            • Service contract period is for 2 years and renewable as per the standard Terms & Conditions.<br/>
+            • Working hours as per Saudi Labor Law.<br/>
+            • Dataflow, Pro-Metric & Classification charges will be reimbursed by the client before candidate’s arrival
+            at Actuals<br/>
+            • Necessary Job Order to be obtained by the client from the respective consulates<br/>
+            • 100 % of the invoiced amount to be paid before candidate’s arrival<br/>
+            • VAT and any Additional Government Taxes and Government Fee will be applicable.<br/>
+            • This quotation is Valid for 7 Days from the issued date.<br/>
 
             For <strong>LEADING INTERNATIONAL RECRUITMENT CORPORATION</strong>
 
@@ -221,6 +240,151 @@
 
     </footer>
 </div>
+<div class="page_break"></div>
+<div class="page1">
+    <header>
+
+        <img style="width: 100%" src="http://alwatnia.com.sa/demo/linrcopdf/head.jpg">
+
+    </header>
+    <main>
+
+        <table style="line-height: 24px; border: none;">
+
+            <tbody>
+            <tr>
+                <td style="font-size: 12px; vertical-align: middle;">إلى
+                    <span class="en"> :</span>
+                </td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+
+                <td style="font-size: 12px; vertical-align: middle;">مرجع رقم<span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+
+            </tr>
+            <tr>
+                <td style="font-size: 12px; vertical-align: middle;">عناية:</td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+                <td style="font-size: 12px; vertical-align: middle;">تاريخ <span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+            </tr>
+            <tr>
+                <td style="font-size: 12px; vertical-align: middle;">هاتف <span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+                <td style="font-size: 12px; vertical-align: middle;">المجال <span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+            </tr>
+            <tr>
+                <td style="font-size: 12px; vertical-align: middle;">موبايل <span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+                <td style="font-size: 12px; vertical-align: middle;">عرض سعر رقم<span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+            </tr>
+            <tr>
+                <td style="font-size: 12px; vertical-align: middle;">إيميل<span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span
+                            class="en">............................</span></td>
+                <td style="font-size: 12px; vertical-align: middle;">عدد الصفحات<span class="en"> :</span></td>
+                <td style="font-size: 12px; vertical-align: middle;"><span class="en">1</span></td>
+            </tr>
+
+
+            </tbody>
+        </table>
+
+        <p>
+            <strong>عزيزي السيد
+                <span class="en">/ ............................. ،</span>
+            </strong> <br/>
+            شكراً لإعطائنا الفرصة لتقديم عرض السعر هذا لشركتكم القيمة<span class="en">.</span> بناءً على متطلباتك<span class="en">،</span> فإن عرضنا التنافسي يتضمن
+            الآتي<span class="en">.</span> يرجى ملاحظة أن الرسوم أدناه تتعلق بتوفير خدمات التوظيف والتأشيرة لشركتكم الموقرة<span class="en">.</span>
+        </p>
+        <table style="line-height: 24px; border: none;">
+
+            <tbody>
+            <tr>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    المهنة
+                </td>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    النوع
+                </td>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    المؤهل
+                </td>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    العدد
+                </td>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    الجنسبة
+                </td>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    الراتب <span class="en">(</span>ريال سعودي<span class="en">)</span>
+                </td>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    رسوم التوظيف
+                    لكل مرشح
+
+                </td>
+                <td style="background: #999;font-size: 12px; vertical-align: middle; text-align: center">
+                    رسوم معالجة التأشيرة
+                    لكل مرشح
+                    <span class="en">( </span>
+                    دولار أمريكي
+                    <span class="en"> $)</span>
+
+                </td>
+            </tr>
+            <tr>
+                <td style="font-size: 12px; vertical-align: middle;">GENERAL NURSES</td>
+                <td style="font-size: 12px; vertical-align: middle;">FEMALE</td>
+                <td style="font-size: 12px; vertical-align: middle;">BACHELOR</td>
+                <td style="font-size: 12px; vertical-align: middle;"><span class="en">15</span></td>
+                <td style="font-size: 12px; vertical-align: middle;">PHILIPPINES</td>
+                <td style="font-size: 12px; vertical-align: middle;"><span class="en">3500+300+</span>
+                    Transport
+                    <span class="en">&</span>
+                    Accommodation
+                </td>
+                <td style="font-size: 12px; vertical-align: middle;">ONE MONTH SALARY</td>
+                <td style="font-size: 12px; vertical-align: middle;"><span class="en">$750</span></td>
+            </tr>
+            </tbody>
+        </table>
+
+        <p style="line-height: 24px;">
+            <strong>الأحكام والشروط<span class="en">:</span> </strong><br/>
+            <span class="en">•</span>	تخضع جميع الشروط والأحكام للعقد الموقع<span class="en">.</span> <br/>
+            <span class="en">•</span>	يتم توفير الإقامة والنقل من جانب العميل<span class="en">.</span><br/>
+            <span class="en">•</span>	يتم توفير تذاكر الطيران من جانب العميل<span class="en">.</span><br/>
+            <span class="en">•</span>	ساعات العمل حسب قانون العمل السعودي<span class="en">.</span><br/>
+            <span class="en">•</span>	سيتم دفع رسوم الداتا فلو والتصنيف والبرومتريك من قبل العميل قبل وصول المرشح<span class="en">.</span><br/>
+            <span class="en">•</span>	يتم تصديق العروض الوظيفية المقدمة من جانب العميل عبر القنصليات المعنية<span class="en">.</span><br/>
+            <span class="en">•</span>	يتم دفع 100٪ من المبلغ المثبت في الفاتورة قبل وصول المرشح<span class="en">.</span><br/>
+            <span class="en">•</span>	سيتم تطبيق ضريبة القيمة المضافة وأي ضرائب حكومية إضافية ورسوم حكومية<span class="en">.</span><br/>
+            <span class="en">•</span>	عرض السعر المرسل صالح لمدة 7 أيام من تاريخ إصداره<span class="en">.</span><br/>
+
+
+            <strong>شركة التوظيف – ليناركو</strong>
+
+        </p>
+
+    </main>
+    <footer>
+
+        <img src="http://alwatnia.com.sa/demo/linrcopdf/foot.jpg" width="100%"/>
+
+    </footer>
+</div>
+
 
 </body>
 </html>
