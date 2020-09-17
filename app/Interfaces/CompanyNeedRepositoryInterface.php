0067c46fe0fa19ad2199193caf212e2b063577cb
Rania <raniaahmed119@gmail.com>
2020-09-17 11:29:14 +0200

    Delete companyNeeds

--- app/Interfaces/CompanyNeedRepositoryInterface.php
+++ app/Interfaces/CompanyNeedRepositoryInterface.php
@@ -12,45 +12,22 @@ namespace App\Interfaces;
 interface CompanyNeedRepositoryInterface
 {
 
-    /** View All Company Need */
+    /** View All CompanyNeed */
     public function index($company_id);
 
     /** Get Employee Types */
     public function getEmployeementtypes($sector_id);
 
-    /** Create Company Need */
+    /** Create CompanyNeed */
     public function create($company_id);
 
-    /** Store Company Need */
+    /** Store CompanyNeed */
     public function store($request);
 
-    /** Show One Company Need */
-    public function show($company);
-
-    /** Submit Edit Company Need */
+    /** Submit Edit CompanyNeed */
     public function update($request , $companyNeed);
 
-    /** Delete Company Need */
-    public function destroy($company);
-
-
-    /** Confirm Connected */
-    public function confirmConnected($company_id);
-
-//    /** Cancel Confirm Connected */
-//    public function cancelConfirmConnected($company_id);
-
-    /** Confirm Interview */
-    public function confirmInterview($company_id);
-
-    /** Confirm Need */
-    public function confirmNeed($company_id);
-
-    /** Confirm Contract */
-    public function confirmContract($company_id);
-
-
-
-
+    /** Delete CompanyNeed */
+    public function destroy($companyNeed);
 
 }
--- app/Repositories/CompanyNeedRepository.php
+++ app/Repositories/CompanyNeedRepository.php
@@ -41,7 +41,7 @@ class CompanyNeedRepository implements CompanyNeedRepositoryInterface
 
     /** View All CompanyNeeds */
     public function index($company_id){
-        return $this->company_need_model::where('company_id' , $company_id)->paginate(20);
+        return $this->company_need_model::where('company_id' , $company_id)->with('company')->paginate(20);
     }
 
     /** Get Employee Types */
@@ -102,13 +102,6 @@ class CompanyNeedRepository implements CompanyNeedRepositoryInterface
     }
 
 
-    /** Show One CompanyNeed */
-    public function show($company)
-    {
-        return $this->companyRepositoryinterface->show($company);
-    }
-
-
     /** Submit Edit CompanyNeed */
     public function update($request , $companyNeed){
         //dd($request->data_flow);
@@ -153,121 +146,16 @@ class CompanyNeedRepository implements CompanyNeedRepositoryInterface
 
     }
 
-    /** Delete Role */
-    public function destroy($company){
-//        dd($company->companyDesignatedcontacts);
-        foreach ($company->companyDesignatedcontacts as $companyDesignatedcontact){
-            $companyDesignatedcontact->delete();
-        }
 
-        foreach ($company->companyMeetings as $companyMeeting){
-            $companyMeeting->delete();
-        }
-        $company->delete();
+    /** Delete CompanyNeed */
+    public function destroy($companyNeed){
+        //Alert::success('warning', trans('dashboard.Are You Sure ?'));
+        $companyNeed->delete();
 
-        $this->addLog(auth()->id() , $company->id , 'companies' , 'تم حذف شركة' , 'Company has been deleted');
+        $this->addLog(auth()->id() , $companyNeed->id , 'company_needs' , 'تم حذف احتياج للشركة ' , 'CompanyNeed has been deleted');
 
         Alert::success('success', trans('dashboard. deleted successfully'));
-        return redirect(route('companies.index'));
-    }
-
-    /** Confirm Connected */
-    public function confirmConnected($company_id){
-        $company = $this->company_model::findOrFail($company_id);
-
-        if ($company->confirm_connected == 0){
-            $company->update([
-                'confirm_connected' => 1,
-                'confirm_connected_user_id' => auth()->id(),
-            ]);
-            $this->addLog(auth()->id() , $company->id , 'companies' , 'تم تأكيد اتصال الشركة   ' , 'Company contact confirmed');
-            return trans('dashboard.Company contact confirmed');
-        }
-        elseif ($company->confirm_connected == 1){
-            $company->update([
-                'confirm_connected' => 0,
-                'confirm_connected_user_id' => auth()->id(),
-            ]);
-            $this->addLog(auth()->id() , $company->id , 'companies' , 'تم إلغاء تأكيد اتصال الشركة  ' , 'The Company contact confirmation has been canceled');
-            return trans('dashboard.The Company contact confirmation has been canceled');
-        }
-
-    }
-
-//    /** Cancel Confirm Connected */
-//    public function cancelConfirmConnected($company_id){
-//        $company = $this->company_model::findOrFail($company_id);
-//        $company->update([
-//            'confirm_connected' => 0,
-//            'confirm_connected_user_id' => auth()->id(),
-//        ]);
-//
-//        $this->addLog(auth()->id() , $company->id , 'companies' , 'تم إلغاء تأكيد اتصال الشركة  ' , 'The Company contact confirmation has been canceled');
-//        return trans('dashboard.Confirmed Connected');
-//    }
-
-    /** Confirm Interview */
-    public function confirmInterview($company_id){
-        $company = $this->company_model::findOrFail($company_id);
-
-        if ($company->confirm_interview == 0){
-            if (count($company->companyMeetings)){
-                $company->update([
-                    'confirm_interview' => 1,
-                    'confirm_interview_user_id' => auth()->id(),
-                ]);
-                $this->addLog(auth()->id() , $company->id , 'companies' , 'تم تأكيد مقابلة الشركة' , 'The company interview was confirmed');
-                return trans('dashboard.The company interview was confirmed');
-            }
-            return trans('dashboard.No interview has been added');
-        }
-        elseif ($company->confirm_interview == 1){
-            $company->update([
-                'confirm_interview' => 0,
-                'confirm_interview_user_id' => auth()->id(),
-            ]);
-            $this->addLog(auth()->id() , $company->id , 'companies' , 'تم إلغاء  مقابلة الشركة' , 'Company interview canceled');
-            return trans('dashboard.Company interview canceled');
-        }
-
-    }
-
-    /** Confirm Need */
-    public function confirmNeed($company_id){
-        $company = $this->company_model::findOrFail($company_id);
-
-        if ($company->confirm_interview == 0){
-            $company->update([
-                'confirm_interview' => 1,
-                'confirm_interview_user_id' => auth()->id(),
-            ]);
-            $this->addLog(auth()->id() , $company->id , 'companies' , 'تم تأكيد مقابلة الشركة' , 'The company interview was confirmed');
-            return trans('dashboard.The company interview was confirmed');
-        }
-
-
-    }
-
-    /** Confirm Contract */
-    public function confirmContract($company_id){
-        $company = $this->company_model::findOrFail($company_id);
-
-        if ($company->confirm_contract == 0){
-            $company->update([
-                'confirm_contract' => 1,
-                'confirm_contract_user_id' => auth()->id(),
-            ]);
-            $this->addLog(auth()->id() , $company->id , 'companies' , 'تم تأكيد التعاقد مع الشركة ' , 'The contract has been confirmed with the company');
-            return trans('dashboard.The contract has been confirmed with the company');
-        }
-        elseif ($company->confirm_contract == 1){
-            $company->update([
-                'confirm_contract' => 0,
-                'confirm_contract_user_id' => auth()->id(),
-            ]);
-            $this->addLog(auth()->id() , $company->id , 'companies' , 'تم إلغاء التعاقد مع الشركة  ' , 'The contract with the company has been canceled');
-            return trans('dashboard.The contract with the company has been canceled');
-        }
+        return redirect(route('company_needs.index' , $companyNeed->company_id));
     }
 
 }
